e92c74116833fc377b13cf03c52b17929bb2eb1b
YUBIN CHOE <cybin050300@naver.com>
2024-07-06 19:52:54 +0900

    fix : 고객의소리 조회 필터링

--- src/components/CRM/VoiceList.jsx
+++ src/components/CRM/VoiceList.jsx
@@ -1,49 +1,68 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import VoiceItem from "./VoiceItem"; // VoiceItem 컴포넌트는 각 소리 항목을 표시할 예정입니다.
-import VoiceDetail from "./VoiceDetail";
-import "../../styles/VoiceList.css"; // VoiceList에 대한 CSS 파일
+import VoiceItem from "./VoiceItem";
+import "../../styles/VoiceList.css";
+import Button from "../Button";
 
 const VoiceList = () => {
   const [voices, setVoices] = useState([]);
-  const [selectedVoice, setSelectedVoice] = useState(null); // 선택된 소리 상태를 관리합니다.
+  const [filter, setFilter] = useState("전체");
 
   useEffect(() => {
-    // 서버에서 고객의 소리 리스트를 가져오는 함수
     const fetchVoices = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:9000/api/v1/intrabucks/customer/voiceList"
+          "http://localhost:9000/api/v1/intrabucks/customer/voice"
         );
-        setVoices(response.data);
+        const sortedVoices = response.data.sort(
+          (a, b) => new Date(b.voiceDate) - new Date(a.voiceDate)
+        );
+        setVoices(sortedVoices);
       } catch (error) {
         console.error("고객의 소리 리스트를 가져오는 중 오류 발생:", error);
       }
     };
 
     fetchVoices();
-  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
+  }, []);
 
-  // 소리 항목을 클릭했을 때 선택된 소리 상태를 업데이트합니다.
-  const handleVoiceClick = (voice) => {
-    setSelectedVoice(voice);
-  };
+  const filteredVoices =
+    filter === "전체"
+      ? voices
+      : filter === "나의 소리"
+      ? voices.filter(
+          (voice) => voice.voiceState === null || voice.voiceState === "미처리"
+        )
+      : voices.filter((voice) => voice.voiceState === "처리완료");
 
   return (
     <div className="container">
       <h2>고객의 소리 리스트</h2>
+      <div className="filter-buttons">
+        <Button
+          className={filter === "전체" ? "active" : ""}
+          onClick={() => setFilter("전체")}
+        >
+          전체
+        </Button>
+        <Button
+          className={filter === "나의 소리" ? "active" : ""}
+          onClick={() => setFilter("나의 소리")}
+        >
+          신규
+        </Button>
+        <Button
+          className={filter === "답변 확인" ? "active" : ""}
+          onClick={() => setFilter("답변 확인")}
+        >
+          완료
+        </Button>
+      </div>
       <div className="voice-list">
-        {/* 모든 소리 항목을 VoiceItem 컴포넌트로 렌더링합니다. */}
-        {voices.map((voice) => (
-          <VoiceItem
-            key={voice.voiceId}
-            voice={voice}
-            onClick={() => handleVoiceClick(voice)}
-          />
+        {filteredVoices.map((voice) => (
+          <VoiceItem key={voice.voiceId} voice={voice} />
         ))}
       </div>
-      {/* 선택된 소리가 있을 경우 VoiceDetail 컴포넌트를 렌더링합니다. */}
-      {selectedVoice && <VoiceDetail voice={selectedVoice} />}
     </div>
   );
 };
