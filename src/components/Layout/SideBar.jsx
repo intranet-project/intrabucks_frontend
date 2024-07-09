620b42ec52ea0dfa18e8cea9b7feba42342365bf
YUBIN CHOE <cybin050300@naver.com>
2024-07-09 13:25:51 +0900

    fix : Page.css추가 및 적용, SideBar고정, 화면 크기 고정, 화면 내 스크롤 적용

--- src/components/Layout/SideBar.jsx
+++ src/components/Layout/SideBar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../../styles/Sidebar.css';
-import logoImg from '../../images/BlackBuckGrouwareLogo.png'; // 이미지 파일 import
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Sidebar.css";
+import logoImg from "../../images/BlackBuckGrouwareLogo.png"; // 이미지 파일 import
 
 const SideBar = () => {
   const [activeMenu, setActiveMenu] = useState(null); // 현재 활성화된 상위 메뉴 상태 관리
@@ -12,8 +12,8 @@ const SideBar = () => {
       setActiveMenu(null); // 이미 활성화된 메뉴를 누르면 닫음
     } else {
       setActiveMenu(menu); // 새로운 메뉴를 누르면 해당 메뉴를 열고 다른 메뉴는 닫음
-      if (menu === '전자결재') {
-        navigate('/approvalPage'); // 전자결재 클릭 시 이동할 페이지
+      if (menu === "전자결재") {
+        navigate("/approvalPage"); // 전자결재 클릭 시 이동할 페이지
       }
     }
   };
@@ -21,75 +21,74 @@ const SideBar = () => {
   // 인사관리
   const handleEmployeeListClick = (e) => {
     e.stopPropagation(); // 이벤트 버블링 방지
-    navigate('/employee-list');
+    navigate("/employee-list");
   };
 
-// 부서관리
+  // 부서관리
   const handleDepartmentClick = (e) => {
     e.stopPropagation(); // 이벤트 버블링 방지
-    navigate('/department');
+    navigate("/department");
   };
 
-// 퇴사관리
+  // 퇴사관리
   const handleQuitterListClick = (e) => {
     e.stopPropagation(); // 이벤트 버블링 방지
-    navigate('/quitter-list');
+    navigate("/quitter-list");
   };
 
   // 매출관리
   const handleSalesManagementClick = (e) => {
     e.stopPropagation(); // 이벤트 버블링 방지
-    navigate('/sales-management');
+    navigate("/sales-management");
   };
-
 
   //재고관리 연결
   const handleStockManagementClick = (e) => {
     e.stopPropagation();
-    navigate('/stockListPage');
-  }
+    navigate("/stockListPage");
+  };
 
   // 매장관리
   const handleStoreListClick = (e) => {
     e.stopPropagation();
-    navigate('/storeListPage');
-  }
+    navigate("/storeListPage");
+  };
 
   //발주관리 연결
   const handlePurchaseManagementClick = (e) => {
     e.stopPropagation();
-    navigate('/purchaseListPage');
-  }
+    navigate("/purchaseListPage");
+  };
 
   // 메뉴관리
   const handleMenuListClick = (e) => {
     e.stopPropagation();
-    navigate('/menuListPage');
-  }
+    navigate("/menuListPage");
+  };
 
-    //전자결재 메인연결
-    const handleApprovalManagementClick = (e) => {
-      e.stopPropagation();
-      navigate('/approvalPage');
-    }
-  
-    //전자결재 연결
-    const handleApprovalPageManagementClick = (e) => {
-      e.stopPropagation();
-      navigate('/approvalPage');
-    }
-  
-    // 전자결재라인 생성
-    const handleApprovalLineManagementClick = (e) => {
-      e.stopPropagation();
-      navigate('/approvalLine');
-    }
-  
-    // 전자결재대기함 생성
-    const handleApprovalWaitingManagementClick = (e) => {
-      e.stopPropagation();
-      navigate('/approvalWaiting');
-    }  
+  //전자결재 메인연결
+  const handleApprovalManagementClick = (e) => {
+    e.stopPropagation();
+    navigate("/approvalPage");
+  };
+
+  //전자결재 연결
+  const handleApprovalPageManagementClick = (e) => {
+    e.stopPropagation();
+    navigate("/approvalPage");
+  };
+
+  // 전자결재라인 생성
+  const handleApprovalLineManagementClick = (e) => {
+    e.stopPropagation();
+    navigate("/approvalLine");
+  };
+
+  // 전자결재대기함 생성
+  const handleApprovalWaitingManagementClick = (e) => {
+    e.stopPropagation();
+    navigate("/approvalWaiting");
+  };
 
   //CRM 연결
   const handleVoiceListClick = (e) => {
@@ -99,32 +98,33 @@ const SideBar = () => {
 
   const handleAnswerClick = (e) => {
     e.stopPropagation();
-    navigate("/voiceAnswer");
+    navigate("/voiceListPage");
   };
-
 
   return (
     <nav className="sideBar">
       <img src={logoImg} alt="Logo" />
       <ul>
         <li
-          className={`sideBar-item ${activeMenu === '홈' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('홈')}
+          className={`sideBar-item ${activeMenu === "홈" ? "active" : ""}`}
+          onClick={() => handleSubMenuToggle("홈")}
         >
           <span className="menu-text">홈</span>
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '로그인' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('로그인')}
+          className={`sideBar-item ${activeMenu === "로그인" ? "active" : ""}`}
+          onClick={() => handleSubMenuToggle("로그인")}
         >
           <span className="menu-text">로그인</span>
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '인사관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('인사관리')}
+          className={`sideBar-item ${
+            activeMenu === "인사관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("인사관리")}
         >
           <span className="menu-text">인사관리</span>
-          {activeMenu === '인사관리' && (
+          {activeMenu === "인사관리" && (
             <ul className="submenu">
               <li className="sideBar-subitem" onClick={handleEmployeeListClick}>
                 직원관리
@@ -139,13 +139,18 @@ const SideBar = () => {
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '매출관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('매출관리')}
+          className={`sideBar-item ${
+            activeMenu === "매출관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("매출관리")}
         >
           <span className="menu-text">매출관리</span>
-          {activeMenu === '매출관리' && (
+          {activeMenu === "매출관리" && (
             <ul className="submenu">
-              <li className="sideBar-subitem" onClick={handleSalesManagementClick}>
+              <li
+                className="sideBar-subitem"
+                onClick={handleSalesManagementClick}
+              >
                 매출 관리
               </li>
             </ul>
@@ -153,24 +158,31 @@ const SideBar = () => {
         </li>
 
         <li
-          className={`sideBar-item ${activeMenu === '재고관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('재고관리')}
+          className={`sideBar-item ${
+            activeMenu === "재고관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("재고관리")}
         >
           <span className="menu-text">재고관리</span>
-          {activeMenu === '재고관리' && (
+          {activeMenu === "재고관리" && (
             <ul className="submenu">
-              <li className="sideBar-subitem" onClick={handleStockManagementClick}>
+              <li
+                className="sideBar-subitem"
+                onClick={handleStockManagementClick}
+              >
                 재고 관리
               </li>
             </ul>
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '매장관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('매장관리')}
+          className={`sideBar-item ${
+            activeMenu === "매장관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("매장관리")}
         >
           <span className="menu-text">매장관리</span>
-          {activeMenu === '매장관리' && (
+          {activeMenu === "매장관리" && (
             <ul className="submenu">
               <li className="sideBar-subitem" onClick={handleStoreListClick}>
                 매장 관리
@@ -179,24 +191,31 @@ const SideBar = () => {
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '발주관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('발주관리')}
+          className={`sideBar-item ${
+            activeMenu === "발주관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("발주관리")}
         >
           <span className="menu-text">발주관리</span>
-          {activeMenu === '발주관리' && (
+          {activeMenu === "발주관리" && (
             <ul className="submenu">
-              <li className="sideBar-subitem" onClick={handlePurchaseManagementClick}>
+              <li
+                className="sideBar-subitem"
+                onClick={handlePurchaseManagementClick}
+              >
                 발주 관리
               </li>
             </ul>
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '메뉴관리' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('메뉴관리')}
+          className={`sideBar-item ${
+            activeMenu === "메뉴관리" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("메뉴관리")}
         >
           <span className="menu-text">메뉴관리</span>
-          {activeMenu === '메뉴관리' && (
+          {activeMenu === "메뉴관리" && (
             <ul className="submenu">
               <li className="sideBar-subitem" onClick={handleMenuListClick}>
                 메뉴 관리
@@ -221,29 +240,39 @@ const SideBar = () => {
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '협업게시판' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('협업게시판')}
+          className={`sideBar-item ${
+            activeMenu === "협업게시판" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("협업게시판")}
         >
           <span className="menu-text">협업게시판</span>
-          {activeMenu === '협업게시판' && (
+          {activeMenu === "협업게시판" && (
             <ul className="submenu">
-              <li className="sideBar-subitem" onClick={handleSalesManagementClick}>
+              <li
+                className="sideBar-subitem"
+                onClick={handleSalesManagementClick}
+              >
                 협업게시판
               </li>
             </ul>
           )}
         </li>
         <li
-          className={`sideBar-item ${activeMenu === '전자결재' ? 'active' : ''}`}
-          onClick={() => handleSubMenuToggle('전자결재')}
+          className={`sideBar-item ${
+            activeMenu === "전자결재" ? "active" : ""
+          }`}
+          onClick={() => handleSubMenuToggle("전자결재")}
         >
-          <span className="menu-text" onClick={() => handleSubMenuToggle('전자결재')}>전자결재</span>
-         
+          <span
+            className="menu-text"
+            onClick={() => handleSubMenuToggle("전자결재")}
+          >
+            전자결재
+          </span>
         </li>
       </ul>
     </nav>
-
   );
-}
+};
 
 export default SideBar;
--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import EmployeeList from '../components/Employee/EmployeeList';
-import Employee from '../components/Employee/Employee';
-import Department from '../components/Department/Department';
-import QuitterList from '../components/Quitter/QuitterList';
-import Quitter from '../components/Quitter/Quitter';
-import EmployeeUpdate from '../components/Employee/EmployeeUpdate';
-import QuitterUpdate from '../components/Quitter/QuitterUpdate';
-import PurchaseListPage from '../pages/PurchaseListPage';
-import StockListPage from '../pages/StockListPage';
+import EmployeeList from "../components/Employee/EmployeeList";
+import Employee from "../components/Employee/Employee";
+import Department from "../components/Department/Department";
+import QuitterList from "../components/Quitter/QuitterList";
+import Quitter from "../components/Quitter/Quitter";
+import EmployeeUpdate from "../components/Employee/EmployeeUpdate";
+import QuitterUpdate from "../components/Quitter/QuitterUpdate";
+import PurchaseListPage from "../pages/PurchaseListPage";
+import StockListPage from "../pages/StockListPage";
 import AddStock from "./Stock/AddStockModal";
 import ApprovalPage from "../pages/ApprovalPage";
 import VoiceList from "../components/CRM/VoiceList";
@@ -24,43 +24,41 @@ import ApprovalSideBar from "./Approval/ApprovalSideBar";
 import ApprovalUpdate from "./Approval/ApprovalUpdate";
 import ApprovalWaitingAll from "../pages/ApprovalWaitingAll";
 import ApprovalGetAll from "../pages/ApprovalGetAll";
-
+import VoiceListPage from "../pages/VoiceListPage";
 
 const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/employee-list" element={<EmployeeList />} />
+      <Route path="/employee-update/:empId" element={<EmployeeUpdate />} />
+      <Route path="/employee" element={<Employee />} />
+      <Route path="/department" element={<Department />} />
+      <Route path="/quitter-list" element={<QuitterList />} />
+      <Route path="/quitter-update" element={<QuitterUpdate />} />
+      <Route path="/quitter" element={<Quitter />} />
+      <Route path="/purchaseListPage" element={<PurchaseListPage />} />
+      <Route path="/stockListPage" element={<StockListPage />} />
+      <Route path="/approvalPage" element={<ApprovalPage />} />
+      <Route path="/sales-management" element={<SalesListPage />} />
+      <Route path="/storeListPage" element={<StoreListPage />} />
+      <Route path="/menuListPage" element={<MenuListPage />} />
+      <Route path="/voiceListPage" element={<VoiceListPage />} />
 
-    return (
-        <Routes>
-            <Route path="/employee-list" element={<EmployeeList />} />
-            <Route path="/employee-update/:empId" element={<EmployeeUpdate />} />
-            <Route path="/employee" element={<Employee />} />
-            <Route path="/department" element={<Department />} />
-            <Route path="/quitter-list" element={<QuitterList />} />
-            <Route path="/quitter-update" element={<QuitterUpdate />} />
-            <Route path="/quitter" element={<Quitter />} />
-            <Route path="/purchaseListPage" element={<PurchaseListPage />} />
-            <Route path="/stockListPage" element={<StockListPage />} />
-            <Route path="/approvalPage" element={<ApprovalPage />} />
-            <Route path="/sales-management" element={<SalesListPage />} />
-            <Route path="/storeListPage" element={<StoreListPage />} />
-            <Route path="/menuListPage" element={<MenuListPage />} />
+      <Route path="/AddStock" element={<AddStock />} />
+      <Route path="/DetailStock" element={<AddStock />} />
 
-            <Route path="/AddStock" element={<AddStock />} />
-            <Route path="/DetailStock" element={<AddStock />} />
+      <Route path="/voiceList" element={<VoiceList />} />
+      <Route path="/voiceAnswer" element={<VoiceAnswer />} />
 
-            <Route path="/voiceList" element={<VoiceList />} />
-            <Route path="/voiceAnswer" element={<VoiceAnswer />} />
-
-            <Route path="/approval" element={<Approval />} />
-            <Route path="/ApprovalLine" element={<ApprovalLine />} />
-            <Route path="/approvalWaiting" element={<ApprovalWaiting />} />
-            <Route path="/approvalUpdate" element={<ApprovalUpdate />} />
-            <Route path="/approvalWaitingAll" element={<ApprovalWaitingAll />} />
-            <Route path="/approvalGetAll" element={<ApprovalGetAll />} />
-            <Route path="/approvalSideBar" element={<ApprovalSideBar />} />
-
-        </Routes>
-    );
-
-}
+      <Route path="/approval" element={<Approval />} />
+      <Route path="/ApprovalLine" element={<ApprovalLine />} />
+      <Route path="/approvalWaiting" element={<ApprovalWaiting />} />
+      <Route path="/approvalUpdate" element={<ApprovalUpdate />} />
+      <Route path="/approvalWaitingAll" element={<ApprovalWaitingAll />} />
+      <Route path="/approvalGetAll" element={<ApprovalGetAll />} />
+      <Route path="/approvalSideBar" element={<ApprovalSideBar />} />
+    </Routes>
+  );
+};
 
 export default Routers;
